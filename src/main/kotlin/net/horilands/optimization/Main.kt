a2a265097b6ad1de7b50c8d9d90a8bad38a9640a
Swiiz <swiizytb@gmail.com>
2021-03-01 01:15:29 +0100

    Added .gitignore

---
+++ src/main/kotlin/net/horilands/optimization/Main.kt
@@ -0,0 +1,40 @@
+package net.horilands.optimization
+
+import org.bukkit.event.Cancellable
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockEvent
+import org.bukkit.event.block.BlockFormEvent
+import org.bukkit.event.block.BlockFromToEvent
+import org.bukkit.event.block.BlockPhysicsEvent
+import org.bukkit.event.entity.EntityChangeBlockEvent
+import org.bukkit.plugin.java.JavaPlugin
+
+
+class Main : JavaPlugin(), Listener {
+
+    override fun onEnable() {
+        server.pluginManager.registerEvents(this, this)
+    }
+
+    fun c(e: BlockEvent) {
+        if (e is Cancellable) (e as Cancellable).isCancelled = true
+    }
+
+    @EventHandler
+    fun physicsEvent(e: BlockPhysicsEvent) = c(e)
+
+    @EventHandler
+    fun liq(e: BlockFromToEvent) = c(e)
+
+    @EventHandler
+    fun cobble(e: BlockFormEvent) = c(e)
+
+    @EventHandler
+    fun sand(e: EntityChangeBlockEvent) {
+        if (e.block.type.hasGravity()) {
+            e.isCancelled = true
+            e.block.state.update(true, false)
+        }
+    }
+}
