b9c33c3bfbbeb1b387bcb405c4eb691fd188b474
Swiiz <swiizytb@gmail.com>
2021-03-02 14:31:23 +0100

    Credit + clean code

--- src/main/kotlin/net/horilands/optimization/Main.kt
+++ src/main/kotlin/net/horilands/optimization/Main.kt
@@ -10,12 +10,12 @@ import org.bukkit.event.block.BlockPhysicsEvent
 import org.bukkit.event.entity.EntityChangeBlockEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-
+/**
+ * @author Swiiz ( for horilands.net )
+ */
 class Main : JavaPlugin(), Listener {
 
-    override fun onEnable() {
-        server.pluginManager.registerEvents(this, this)
-    }
+    override fun onEnable() = server.pluginManager.registerEvents(this, this)
 
     fun c(e: BlockEvent) {
         (e as? Cancellable)?.isCancelled = true
